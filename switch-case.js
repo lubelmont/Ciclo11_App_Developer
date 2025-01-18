9c9c05c86cbc16f9dc85262345fdd23e74cd04f0
Arnufo Rodríguez <luis.rodriguez@lubelsoft.com>
2025-01-18 11:06:07 -0600

    switch-case intro

---
+++ switch-case.js
@@ -0,0 +1,29 @@
+
+let expresion = "1"
+
+
+switch (expresion) {
+    case "1":
+        console.log('es 1')
+        console.log('Segunda linea')
+        break;
+    case "2":
+        console.log('es 2')
+        break;
+    default:
+        console.log('Es default')
+}
+
+
+
+let calificacion = "A"
+
+switch(calificacion){
+    case "A":
+        console.log("Excelente")
+    break;
+    default:
+        console.log("No excelente")
+
+}
+
