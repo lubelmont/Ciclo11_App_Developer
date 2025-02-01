246d20293ecf547a8f1daac8da2c5609918f1d2f
Arnufo Rodríguez <luis.rodriguez@lubelsoft.com>
2025-02-01 10:48:59 -0600

    Actualizar peliculas

---
+++ while-do-while_ejercicio.js
@@ -0,0 +1,32 @@
+// Listar las peliculas y su categoria
+// Salida esperada:
+/*
+    🎭 Drama: El Padrino
+    🔥 Acción: Avengers: Endgame
+    🎨 Animación: Buscando a Nemo
+    🎭 Drama: Pulp Fiction
+    🔥 Acción: Gladiador
+    🎨 Animación: Toy Story
+*/
+
+//arrglo = ["🎭","🔥","🎨"];
+//objeto = { "🎭": "Drama", "🔥": "Acción", "🎨": "Animación" };
+
+
+// arreglos = [0,1,2,3,4,5]
+
+const peliculas = [
+    { titulo: "El Padrino", genero: "Drama" },
+    { titulo: "Avengers: Endgame", genero: "Acción" },
+    { titulo: "Buscando a Nemo", genero: "Animación" },
+    { titulo: "Pulp Fiction", genero: "Drama" },
+    { titulo: "Gladiador", genero: "Acción" },
+    { titulo: "Toy Story", genero: "Animación" },
+  ];
+
+
+
+  // Ejercicio 1: Buscar pelicula
+let busqueda = "Toy Story";
+
+
